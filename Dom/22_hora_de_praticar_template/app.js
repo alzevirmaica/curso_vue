b33cfe27ce21ab5d60abca29ca33414573562335
Alzevir Maicá <alzevirm@gmail.com>
2023-07-08 14:58:28 -0300

    adicionando curso

---
+++ Dom/22_hora_de_praticar_template/app.js
@@ -0,0 +1,8 @@
+new Vue ({
+    el: '#desafio',
+    data: {
+        nome: 'Alzevir Maicá',
+        idade: 39,
+        imagem: 'http://files.cod3r.com.br/curso-vue/vue.jpg'
+    }
+})
---
+++ Dom/29_hora_de_praticar_eventos/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: ''
+    },
+    methods: {
+        exibirAlerta() {
+            alert('Eu estou te alertando')
+        }
+    },
+})
